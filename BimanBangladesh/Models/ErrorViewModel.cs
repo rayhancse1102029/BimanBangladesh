78d40ee6003de8c2125ebca74a9159d2cec3eff0
rayhancse15git <abu@baiust.edu.bd>
2020-12-18 12:31:49 +0600

    Add project files.

---
+++ BimanBangladesh/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BimanBangladesh.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
